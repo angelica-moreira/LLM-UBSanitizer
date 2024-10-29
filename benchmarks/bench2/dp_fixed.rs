c138f0274cbf439537114155e28dbb13f8ebedf1
Angelica Moreira <48168649+angelica-moreira@users.noreply.github.com>
2024-10-29 00:15:08 +0000

    removing file

--- benchmarks/bench2/dp_fixed.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let x = 5;
-    let r = &x; // Safe reference to 'x'
-
-    println!("r: {}", *r); // No 'unsafe' block needed
-    // 'x' goes out of scope here, which is fine because we're done using 'r'
-}
